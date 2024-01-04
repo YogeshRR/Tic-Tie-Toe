f1caa11fa97423fb23fa183b50e36c17a7af04a5
YogeshRR <43900564+YogeshRR@users.noreply.github.com>
2024-01-04 20:37:08 +0530

    Tic-Tac-Toe game chapter - 73

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-
+import Player from "./component/player";
 function App() {
 
-
   return (
-    <h1>Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name='player 1' symbol='x' />
+          <Player name='player 2' symbol='0' />
+        </ol>
+        GAMES
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App
