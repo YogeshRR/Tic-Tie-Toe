e7e7deda77b43f4368d87c5bdaca0c56806cca16
YogeshRR <43900564+YogeshRR@users.noreply.github.com>
2024-01-05 14:16:47 +0530

    rendering multi-dimensional lists Chapter 79

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Player from "./component/player";
+import GameBoard from "./component/GameBoard";
 function App() {
 
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name='player 1' symbol='x' />
-          <Player name='player 2' symbol='0' />
+          <Player initialName='player 1' symbol='x' />
+          <Player initialName='player 2' symbol='0' />
         </ol>
-        GAMES
+        <GameBoard />
       </div>
       LOG
     </main>
--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+    const [playerNameVariable, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
     function handleEditClick(checkValue) {
         setIsEditing((editing) => !editing);
     }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
     let playerName = <span className="player-name">
-        {name}
+        {playerNameVariable}
     </span>
     let btnCaption = 'Edit';
     if (isEditing) {
-        playerName = <input type="text" required value={name} />
+        playerName = <input type="text" required onChange={handleChange} value={playerNameVariable} />
     }
     return (
         <li>
