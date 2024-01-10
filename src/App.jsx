e89a7a9a98830568f7bf5168088ceb2676f027f1
YogeshRR <43900564+YogeshRR@users.noreply.github.com>
2024-01-10 20:04:25 +0530

    Chapter 86 Reducing state management and identifying unnecessary state

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import Player from './component/player.jsx';
 import GameBoard from './component/GameBoard.jsx';
 import Log from './component/Log.jsx';
 
+function derivedActivePlayer(gameTurns) {
+
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
-
+  //const [activePlayer, setActivePlayer] = useState('X');
+  const currentPlayer = derivedActivePlayer(gameTurns);
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
 
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = derivedActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
@@ -33,12 +40,12 @@ function App() {
           <Player
             initialName="Player 1"
             symbol="X"
-            isActive={activePlayer === 'X'}
+            isActive={currentPlayer === 'X'}
           />
           <Player
             initialName="Player 2"
             symbol="O"
-            isActive={activePlayer === 'O'}
+            isActive={currentPlayer === 'O'}
           />
         </ol>
         <GameBoard
